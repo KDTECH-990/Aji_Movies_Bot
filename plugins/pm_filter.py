71e8fed631928d33617a6380fdf31353f1bc3e1e
KD Tech [#Offline] <137230575+KDTECH-990@users.noreply.github.com>
2023-08-02 13:01:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,4 +1,4 @@
-import asyncio, re, ast, math, logging
+import asyncio, re, ast, math, logging, random, 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
